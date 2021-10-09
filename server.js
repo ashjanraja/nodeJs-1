da56576c738756a76c74efe4ba741239be58d5dc
ashjanraja <ashjannairat787@gmail.com>
2021-10-09 19:44:12 +0300

    done

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
 const express = require("express");
 
 const app = express();
-const cars  =[
-    {
-        name : "BMW",
-        color : "black",
-    },
-    {
-        name : "Kia",
-        color: "gold",
-    },
-    {
-        name:"towota",
-        color : "green"
-    }
+const cars = [
+  {
+    name: "BMW",
+    color: "black",
+  },
+  {
+    name: "Kia",
+    color: "gold",
+  },
+  {
+    name: "towota",
+    color: "green",
+  },
 ];
 
-app.get("/",  (req, res) => res.send("Hello World"))
-app.get("/cars" , (req , res)=>{
-    res.json(cars);
-})
+app.get("/", (req, res) => res.send("Hello World"));
+app.get("/cars", (req, res) => {
+  res.json(cars);
+});
 
-app.get("/search" , (req , res)=>{
-    const {name, color}=req.query;
+app.get("/search", (req, res) => {
+  const { name, color } = req.query;
 
-    let newArr = cars
+  let newArr = cars;
 
-    if(name){
-        newArr = cars.filter(item => item.name.includes(name))
-    }
-    if(color){
-        newArr = cars.filter(item => item.color.includes(color))
-    }
+  if (name) {
+    newArr = cars.filter((item) => item.name.includes(name));
+  }
+  if (color) {
+    newArr = cars.filter((item) => item.color.includes(color));
+  }
 
-    
-    if(newArr.length === 0){
-        res.status(400).send('400 hello')
-    }
-    else{
-        res.status(200).json(newArr)
-    }
-})
+  if (newArr.length === 0) {
+    res.status(400).send("400 hello");
+  } else {
+    res.status(200).json(newArr);
+  }
+});
 
-app.listen(3000,console.log("Server is running http://localhost:3000"));
+app.listen(
+  process.env.PORT || 5000,
+  console.log("Server is running http://localhost:3000")
+);
